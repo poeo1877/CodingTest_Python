a640538c545d6e6a167c1e7ddfb022ed85cb5aaf
Shin Yongin <67182280+poeo1877@users.noreply.github.com>
2024-04-25 10:30:19 +0900

    [level 2] Title: 행렬의 곱셈, Time: 0.32 ms, Memory: 28.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+# 머신러닝 과목 공부하며 사용하였으나, 실제 코딩테스트에서는 해당 라이브러리
+# 사용할 수 없는 경우가 있다. 다른 방법으로 이 문제를 해결하는 것이 바람직하다.
 def solution(arr1, arr2):
     _arr1 = np.array(arr1)
     _arr2 = np.array(arr2)
